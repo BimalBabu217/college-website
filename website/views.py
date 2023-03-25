94f46017bb73cc3baedc7eb1470b8e9cb82e04d4
Jenin <jenin8282@gmail.com>
2023-03-25 08:56:12 +0530

    homepage setup

--- website/views.py
+++ website/views.py
@@ -4,3 +4,6 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
   return render(request, "website/index.html")
+
+def home_view(request):
+  return render(request, "website/home.html")
