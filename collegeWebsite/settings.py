4db9e1ae16748a1c4eb02f73299117b5cd2b9732
Jenin <jenin8282@gmail.com>
2023-03-25 09:38:14 +0530

    homepage link to website page

--- collegeWebsite/settings.py
+++ collegeWebsite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-py_+ktluyko&#_9y#sp9#im9f^=^o7o8evpka3#&&o&vx95xtg
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app']
+ALLOWED_HOSTS = ['.vercel.app','127.0.0.1']
 
 
 # Application definition
